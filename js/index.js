ae336a80249d2ab6cc0b3cb2a15f7d5c361808b0
sofi131 <sofiasanmartin131@gmail.com>
2024-03-15 18:17:05 +0100

    función celdas vacías

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-var tablero=
+
+var tablero={
+    "00":0,
+    "01":0,
+    "02":0,
+    "03":0,
+    "10":0,
+    "11":0,
+    "12":0,
+    "13":0,
+    "20":0,
+    "21":0,
+    "22":0,
+    "23":0,
+    "30":0,
+    "31":0,
+    "32":0,
+    "33":0,
+};
+
+//array para que ponga las celdas vacías -> por cada celda vacía lo meto en el array
+function celdasVacias() {
+    let arrayCeldasVacias=[];
+    for (const key in tablero) {
+        //si el objeto está vacío, me devuelve las keys de las celdas que están vacías
+        if (tablero[key]==0) {
+            arrayCeldasVacias.push(key);
+        }
+    }
+    return arrayCeldasVacias;
+}
